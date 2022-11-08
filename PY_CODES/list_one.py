3953191916b9434cfcbb6710170d83b196854260
Olatunde Tai-Lawal <114710590+tundeficky@users.noreply.github.com>
2022-11-08 12:53:33 -0800

    list of numbers in python

---
+++ PY_CODES/list_one.py
@@ -0,0 +1,10 @@
+print("**********This is to list the min and max numbers in a variables****\n")
+list_numbers_A = [3,4,5,6]
+list_numbers_B = [-1 ,-2,-3,-4]
+list_numbers_C = [0,0,0,0]
+list_numbers_D = []
+print(f"{max(list_numbers_A)}, {min(list_numbers_A)}\n")
+print(f"{max(list_numbers_B)}, {min(list_numbers_B)}\n")
+print(f"{max(list_numbers_C)}, {min(list_numbers_C)}\n")
+if len(list_numbers_D) == 0:
+    print("none")
